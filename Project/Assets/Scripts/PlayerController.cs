a0c1b7e69870ee5843968089cf70e0521a13d3e5
Damir564 <ddamir.nizamutdinov@yandex.ru>
2022-01-06 21:50:34 +0300

    change public properties to private

--- Project/Assets/Scripts/PlayerController.cs
+++ Project/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 //Carry out do we need a FixedUpdate() or just can use Update()  <-- to-do thing
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 1f; //Movement Speed of the Player
-    public Vector2 movement;         //Movement Axis
-    public Rigidbody2D rb;    //Player Rigidbody Component
+    [SerializeField]
+    private float m_movementSpeed = 1f; //Movement Speed of the Player
+    [SerializeField]
+    private Vector2 m_movement;         //Movement Axis
+    [SerializeField]
+    private Rigidbody2D rb;    //Player Rigidbody Component
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
+        m_movement.x = Input.GetAxis("Horizontal");
+        m_movement.y = Input.GetAxis("Vertical");
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + m_movement * m_movementSpeed * Time.fixedDeltaTime);
     }
 }
